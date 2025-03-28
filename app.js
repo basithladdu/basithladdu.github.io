f4790e83cebd70b9072bae75da229d56e69dfd87
Shaikawaiz01 <ashawaiz3611@gmail.com>
2025-03-28 14:28:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ function loadContent(option) {
     const contentDiv = document.getElementById('content');
     contentDiv.textContent = `${option} - coming soon`;
 }
+
+function expandCard(clickedCard) {
+    document.querySelectorAll('.card').forEach(card => {
+        card.classList.remove('expanded');
+    });
+    clickedCard.classList.add('expanded');
+}
+
+document.addEventListener("scroll", function() {
+    let cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        let cardPosition = card.getBoundingClientRect().top;
+        let screenPosition = window.innerHeight / 1.2;
+        
+        if (cardPosition < screenPosition) {
+            card.classList.add("visible");
+        }
+    });
+});
